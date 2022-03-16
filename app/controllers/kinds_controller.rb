40ec7f8f702278a4643e27bd487274dab6f32cd3
Vinicius Novais <viniciusnovais74@gmail.com>
2022-03-16 15:59:52 -0300

    Authenticate by Diggest

--- app/controllers/kinds_controller.rb
+++ app/controllers/kinds_controller.rb
@@ -1,4 +1,11 @@
 class KindsController < ApplicationController
+  # include ActionController::HttpAuthentication::Basic::ControllerMethods
+  # http_basic_authenticate_with name: "jack", password: "secret"
+  include ActionController::HttpAuthentication::Digest::ControllerMethods
+  USERS = { "jack" => Digest::MD5.hexdigest(["jack","Application","secret"].join(":"))}
+
+  before_action :authenticate
+
   before_action :set_kind, only: [:show, :update, :destroy]
 
   # GET /kinds
@@ -47,6 +54,12 @@ class KindsController < ApplicationController
       @kind = Kind.find(params[:id])
     end
 
+    def authenticate
+      authenticate_or_request_with_http_digest("Application") do |username|
+        USERS[username]
+      end
+    end
+    
     # Only allow a trusted parameter "white list" through.
     def kind_params
       params.require(:kind).permit(:description)
